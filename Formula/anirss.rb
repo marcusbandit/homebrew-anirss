a0d7d6dc80b7bd6f9e45aa0e203651aa41d0c4e6
marcusbandit <marcusbanditten@gmail.com>
2026-05-10 01:14:42 +0200

    anirss 0.2.6: tab-complete feed names containing colons

--- Formula/anirss.rb
+++ Formula/anirss.rb
@@ -1,8 +1,8 @@
 class Anirss < Formula
   desc "Search nyaa.si and hand the result to qBittorrent"
   homepage "https://github.com/marcusbandit/anirss"
-  url "https://github.com/marcusbandit/anirss/archive/refs/tags/v0.2.5.tar.gz"
-  sha256 "ef85e1654a080a0be4883f564d24716f71ee0c55a0a405bc3805972049682f5a"
+  url "https://github.com/marcusbandit/anirss/archive/refs/tags/v0.2.6.tar.gz"
+  sha256 "91e83a33202cb7ae1e7e3535d6ea7a9614bfc7157338bb2b37579356ae47e247"
   license "GPL-3.0-or-later"
   head "https://github.com/marcusbandit/anirss.git", branch: "main"
 
