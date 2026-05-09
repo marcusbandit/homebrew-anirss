adee8b9d2c09dc7d079d40f6753b92024277820f
marcusbandit <marcusbanditten@gmail.com>
2026-05-09 22:18:40 +0200

    anirss 0.1.0

---
+++ Formula/anirss.rb
@@ -0,0 +1,22 @@
+require "language/python"
+
+class Anirss < Formula
+  desc "Search nyaa.si and hand the result to qBittorrent"
+  homepage "https://github.com/marcusbandit/anirss"
+  url "https://github.com/marcusbandit/anirss/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "341d7fd355f090dd4c23830ae000f0c965048934b8446872febcdd1c32d13776"
+  license "GPL-3.0-or-later"
+  head "https://github.com/marcusbandit/anirss.git", branch: "main"
+
+  depends_on "fzf"
+  depends_on "python@3.13"
+
+  def install
+    rewrite_shebang detected_python_shebang, "anirss"
+    bin.install "anirss"
+  end
+
+  test do
+    assert_equal "anirss #{version}", shell_output("#{bin}/anirss --version").strip
+  end
+end
---
+++ README.md
@@ -0,0 +1,22 @@
+# homebrew-anirss
+
+Homebrew tap for [anirss](https://github.com/marcusbandit/anirss).
+
+## Install
+
+```sh
+brew install marcusbandit/anirss/anirss
+```
+
+Or, two-step:
+
+```sh
+brew tap marcusbandit/anirss
+brew install anirss
+```
+
+Bleeding-edge from `main`:
+
+```sh
+brew install --HEAD marcusbandit/anirss/anirss
+```
