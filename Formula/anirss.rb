ec92ee7904060fd1179205fff2b0eebc315a9eeb
marcusbandit <marcusbanditten@gmail.com>
2026-05-10 01:10:13 +0200

    anirss 0.2.5: tab completion fixes (Pattern-A, auto-wipe zcompdump)

--- Formula/anirss.rb
+++ Formula/anirss.rb
@@ -1,8 +1,8 @@
 class Anirss < Formula
   desc "Search nyaa.si and hand the result to qBittorrent"
   homepage "https://github.com/marcusbandit/anirss"
-  url "https://github.com/marcusbandit/anirss/archive/refs/tags/v0.2.4.tar.gz"
-  sha256 "eab8bd26d0ee896e58876a3e1087b1aa19dc76d9da9065090f5eaf64d466623a"
+  url "https://github.com/marcusbandit/anirss/archive/refs/tags/v0.2.5.tar.gz"
+  sha256 "ef85e1654a080a0be4883f564d24716f71ee0c55a0a405bc3805972049682f5a"
   license "GPL-3.0-or-later"
   head "https://github.com/marcusbandit/anirss.git", branch: "main"
 
